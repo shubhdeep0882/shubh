7b45e6c6848109fae8a83bbe9a983695649cefad
shubhdeep0882 <165235814+shubhdeep0882@users.noreply.github.com>
2024-04-17 12:13:34 +1200

    Add files via upload

---
+++ program.py
@@ -0,0 +1,15 @@
+a = 44
+b = 22
+c = 11
+
+print(a - b * c)
+print(a + b * c)
+print(a / b * c)
+print(a / b)
+print("division of number",a/c)
+
+
+first_number = int(input("enter first number:"))
+second_number = int(input("enter second number:"))
+
+print(first_number*second_number)

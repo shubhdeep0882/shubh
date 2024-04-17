75c049c1ab14850b6ec6761346c0b78968773bb0
shubhdeep0882 <165235814+shubhdeep0882@users.noreply.github.com>
2024-04-17 12:13:16 +1200

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,33 @@
+#a = 10
+#b = 5
+
+#if a > b:
+    #print(f"{a} is larger than {b}")
+#else: 
+    #print("that value is not larger")
+
+    #color = "red"
+
+    #guess = input ("Guess my colour: ")
+
+    #if guess == "red":
+        #print("you guessed right!")
+    #else:
+        #print("wrong!")    
+
+grade = int (input("what was your grade?"))
+
+#if grade > 80:
+#    print("A")
+#elif grade >= 70:    
+#    print("B")
+#elif grade >= 60:
+#     print("C") 
+#elif grade >= 50:
+#     print("D") 
+#else:                
+#     print("F")
+
+
+if grade >=50:
+    print("D")

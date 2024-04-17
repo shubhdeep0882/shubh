7627062d0cd36a03590e3fce9dc45542b16bb2dd
shubhdeep0882 <165235814+shubhdeep0882@users.noreply.github.com>
2024-04-17 12:12:46 +1200

    Add files via upload

---
+++ firstno.py
@@ -0,0 +1,57 @@
+firstname = "shubh"
+FirstName = "shubh"
+first_name = "shubh"
+
+first_number = 10j
+second_number = 20
+last_name = "singh"
+print("shubh")
+
+print(type(first_number))
+print(type(second_number))
+print(type(last_name))
+
+
+a = 12j
+b = 23
+c = "singh"
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(False))
+
+#write a program to demostrate the arithmetic operation
+a = 44
+b = 22
+c = 11
+
+print(a - b * c)
+print(a + b * c)
+print(a / b * c)
+print(a / b)
+print("division of number",a/c)
+
+
+first_number = int(input("enter first number:"))
+second_number = int(input("enter second number:"))
+
+print(first_number*second_number)
+
+
+
+
+
+
+   #demo.py
+a = 10
+b = 5
+if (a>b):
+     print("wow")
+else:
+    print("false condition")
+    
+
+
+
+
